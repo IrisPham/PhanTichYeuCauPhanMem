c2b7cc8ccc8e34e904d9613ee5706826593ce538
Thanhgr <thanhb1400727@student.ctu.edu.vn>
2017-04-04 08:39:04 +0700

    Them nut
    by Thanh

--- phantichyeucauphanmem/phantichyeucauphanmem/GUI/frmLogin.cs
+++ phantichyeucauphanmem/phantichyeucauphanmem/GUI/frmLogin.cs
@@ -15,5 +15,10 @@ namespace phantichyeucauphanmem
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //fvfvygbygbyg
+        }
     }
 }
